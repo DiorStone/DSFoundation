b5d211a7895510e73ec8ea0545dde71c4d51213e
mrdaios <mrdaios@gmail.com>
2018-07-06 17:54:48 +0800

    【修改】 * Paging添加

--- Sources/Utils/Paging/Pagingable.swift
+++ Sources/Utils/Paging/Pagingable.swift
@@ -8,7 +8,58 @@ import Foundation
 
 public protocol Pagingable {
 	
-	var currentPage: Int { get set }
-	var pageSize: Int { get set }
-	var hasNextPage: Bool { get set }
+    var paging: Paging { get }
+}
+
+
+extension Pagingable {
+    
+    public var paging: Paging {
+        
+        return Paging()
+    }
+}
+
+/// 分页配置
+public class Paging {
+    
+    /// 当前页码
+    public var currentPage: Int
+    
+    /// 分页大小
+    public var pageSize: Int
+    
+    /// 是否有下一页
+    public var hasNextPage: Bool
+    
+    public init(startPage: Int = PagingConfig.default.startPage,
+                pageSize: Int  = PagingConfig.default.pageSize) {
+        self.currentPage = startPage
+        self.pageSize = pageSize
+        self.hasNextPage = true
+    }
+    
+    /// 重置分页配置
+    ///
+    /// - Parameters:
+    ///   - startPage: startPage
+    ///   - pageSize: pageSize
+    public func reset(startPage: Int = PagingConfig.default.startPage,
+                      pageSize: Int  = PagingConfig.default.pageSize) {
+        self.currentPage = startPage
+        self.pageSize = pageSize
+        self.hasNextPage = true
+    }
+}
+
+/// 分页全局配置信息,可以在app初始化好后配置
+public class PagingConfig {
+    
+    /// 起始页,默认为0.
+    public var startPage: Int = 0
+    
+    /// 分页大小,默认为10.
+    public var pageSize: Int = 10
+    
+    public static let `default`: PagingConfig = PagingConfig()
 }
